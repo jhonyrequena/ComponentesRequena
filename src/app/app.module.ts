789bb7aacbf2a929dbaf175343037ce0109c5301
jhonyrequena <requenajhony@gmail.com>
2023-09-18 10:54:18 -0300

    Creando mis primeros componentes con Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    SidebarComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
